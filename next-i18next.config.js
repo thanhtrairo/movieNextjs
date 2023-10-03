32a911c07a2958286455094b9f513f3a7c030ce3
thanhtrairo <tienthanhcntn789@gmail.com>
2023-10-03 12:38:05 +0700

    fix i18next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'vi'],
+    localePath: path.resolve('public/locales'),
+    react: { useSuspense: false },
   },
 }
